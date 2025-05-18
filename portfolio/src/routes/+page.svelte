8b1a771e80c3c93c57befa270d2c9e8614088c72
Sarah Schurgers <sarah.schurgers@student.pxl.be>
2025-05-18 16:26:39 +0200

    Home page

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,1 +1,58 @@
-<h1>home</h1>
+<style>
+  .name{
+    font-size: 100px;
+    font-weight: 600;
+    color: #353535;
+    text-decoration: none;
+    margin-top: 200px;
+    margin-bottom: 10px;
+  }
+  div{
+    display: flex;    
+    flex-direction: column;
+  }
+  p {
+    margin: 4px 0;
+    font-size: 20px;
+  }
+
+  .socials {
+    display: flex;
+    flex-direction: row;
+    margin-top: 8px;
+    gap: 15px;
+  }
+
+  a {
+    color: #353535;
+    font-size: 1.8rem;
+  }
+
+
+
+</style>
+
+
+<div>
+  <h1 class="name">Sarah Schurgers</h1>
+  <p>
+    Motivated final-year Applied Computer Science student (Switch2IT).
+  </p>
+  <p>
+  Passionate about frontend development, intuitive UI/UX design, and smart data processing.
+  </p>
+  <p>
+  Eager to turn ideas into clean, user-friendly digital experiences.
+  </p>
+  <div class="socials">
+    <a href="https://github.com/SarahSchurgersPXL" target="_blank" aria-label="GitHub">
+      <i class="fab fa-github"></i>
+    </a>
+    <a href="https://www.linkedin.com/in/sarahschurgers/" target="_blank" aria-label="LinkedIn">
+      <i class="fab fa-linkedin"></i>
+    </a>
+    <a href="mailto:sarahschurgers@gmail.com" aria-label="E-mail">
+      <i class="fas fa-envelope"></i>
+    </a>
+  </div>
+</div>
