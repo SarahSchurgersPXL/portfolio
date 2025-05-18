244cf0c2453fd6ff7ec42c1f2d24dee5d260e70c
Sarah Schurgers <sarah.schurgers@student.pxl.be>
2025-05-18 13:13:11 +0200

    icons in navbar

--- portfolio/src/routes/+layout.svelte
+++ portfolio/src/routes/+layout.svelte
@@ -10,10 +10,18 @@
   <div class="nav-container">
     <a href="/" class="nav-link-brand">Sarah Schurgers</a>
     <div class="nav-links {menuOpen ? 'open' : ''}">
-      <a href="/skills" class="nav-link">Skills</a>
-      <a href="/projects" class="nav-link">Projects</a>
-      <a href="/experience" class="nav-link">Experience</a>
-      <a href="/education" class="nav-link">Education</a>    
+      <a href="/skills" class="nav-link">
+        <i class="fas fa-lightbulb"></i> Skills
+      </a>
+      <a href="/projects" class="nav-link">
+        <i class="fas fa-code"></i> Projects
+      </a>
+      <a href="/experience" class="nav-link">
+        <i class="fas fa-briefcase"></i> Experience
+      </a>
+      <a href="/education" class="nav-link">
+        <i class="fas fa-graduation-cap"></i> Education
+      </a>    
     </div>
     <div class="hamburger" on:click={() => menuOpen = !menuOpen}>
       <span></span>
