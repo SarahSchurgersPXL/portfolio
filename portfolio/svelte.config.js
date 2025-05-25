1816bc90612007ae45e9d169a3cc7b11a8eb60a8
Sarah Schurgers <sarah.schurgers@student.pxl.be>
2025-05-25 15:26:15 +0200

    static

--- portfolio/svelte.config.js
+++ portfolio/svelte.config.js
@@ -1,16 +1,25 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-    kit: {
-        adapter: adapter(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: '',
+		},
+
+
 		prerender: {
-			entries: []
+			entries: ['*']  // Vervangt 'default: true'
 		}
-    }
+	}
 };
 
 export default config;
